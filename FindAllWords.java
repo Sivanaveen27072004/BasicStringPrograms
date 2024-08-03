a089e317a1721ede53186f08db8858ab27c1c031
Sivnaveen L. <sivanaveenecemec@gmail.com>
2024-08-03 10:29:29 +0530

    FindAllWords.java

---
+++ FindAllWords.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class FindAllWords
+{
+public static void main(String[] args) {
+	String s="this is java full stack couse";
+	String s1[]=s.split(" ");
+	System.out.println(Arrays.toString(s1));
+}
+}
