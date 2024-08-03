b7d4bc678d230f10334020767f461f319c335a5b
Sivnaveen L. <sivanaveenecemec@gmail.com>
2024-08-03 10:28:15 +0530

    ReverseOfString.java

---
+++ ReverseOfString.java
@@ -0,0 +1,12 @@
+public class ReverseOfString 
+{
+	public static void main(String[] args)
+	{
+		String s1="Sivanaveen";
+		String s2="";
+		for(int i=s1.length()-1;i>=0;i--)
+			s2+=s1.charAt(i);
+		System.out.println(s1);
+		System.out.println(s2);
+	}
+}
