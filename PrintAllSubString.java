0cb9a1db43cff430ad6e3411fb31f20065105ea4
Sivnaveen L. <sivanaveenecemec@gmail.com>
2024-08-06 14:52:05 +0530

    PrintAllSubString.java

---
+++ PrintAllSubString.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class PrintAllSubString
+{
+public static void main(String[]args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the String");
+String s=sc.nextLine();
+for(int i=0;i<s.length();i++)
+{
+for(int j=i+3;i<=s.length();j++)
+{
+String s2=s.substring(i,j);gdhbgdhteu
+ujdj
+yryjsivanaveenndyhtfdm
+dnumjf
+System.out.println(s2);
+}
+}
+}
+}
