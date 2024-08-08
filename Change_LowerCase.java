30f369bd1071ebbde37dd4c0e5a5adcafad86f10
Sivnaveen L. <sivanaveenecemec@gmail.com>
2024-08-08 11:31:52 +0530

    Create Change_LowerCase.java

---
+++ Change_LowerCase.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Change_LowerCase
+{
+public static void main(String [] args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the String in UpperCase");
+String s1=sc.next();
+System.out.println(s1.toLowerCase());
+}
+}
